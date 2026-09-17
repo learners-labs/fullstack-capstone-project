9e4c0ba6aea965857915be7bbb91287d8968d66c
AD <adrian.ext@gmail.com>
2026-09-17 16:06:18 +0800

    BE api search

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -8,6 +8,7 @@ const connectToDatabase = require("./models/db");
 const { loadData } = require("./util/import-mongo/index");
 
 const giftRoutes = require("./routes/giftRoutes");
+const searchRoutes = require("./routes/searchRoutes");
 
 const app = express();
 app.use("*", cors());
@@ -27,7 +28,7 @@ app.use(express.json());
 app.use("/api/gifts", giftRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+app.use("/api/search", searchRoutes);
 
 const pinoHttp = require("pino-http");
 const logger = require("./logger");
