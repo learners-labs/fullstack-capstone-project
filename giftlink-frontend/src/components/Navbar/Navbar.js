cdb88788226752a4b2428bc56dd4671af4e2d598
AD <adrian.ext@gmail.com>
2026-09-17 18:48:55 +0800

    Implement search functionality and style SearchPage.

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -13,13 +14,16 @@ export default function Navbar() {
             <a className="nav-link" href="/home.html">
               Home
             </a>{" "}
-            {/* Link to home.html */}
           </li>
           <li className="nav-item">
             <a className="nav-link" href="/app">
               Gifts
-            </a>{" "}
-            {/* Updated Link */}
+            </a>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/app/search">
+              Search
+            </Link>
           </li>
         </ul>
       </div>
