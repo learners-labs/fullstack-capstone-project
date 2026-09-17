b87f88e96048ba9450c340645830451cbd825958
AD <adrian.ext@gmail.com>
2026-09-17 16:59:57 +0800

    FE mainpage

--- giftlink-frontend/src/config.js
+++ giftlink-frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  backendUrl: process.env.REACT_APP_BACKEND_URL,
+  backendUrl: process.env.REACT_APP_BACKEND_URL || "http://localhost:3060",
 };
 
-console.log(`backendUrl in config.js: ${config.backendUrl}`)
-export {config as urlConfig}
+console.log(`backendUrl in config.js: ${config.backendUrl}`);
+export { config as urlConfig };
